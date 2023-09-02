3c18f286f2d2446e7d2ac6905bff7434a17fb837
David <grayd500@gmail.com>
2023-09-02 15:56:52 -0500

    DRY code

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,11 @@
 // Wrap all code in a call to jQuery
 $(function () {
 
+updateTime();
+updateBlockColors();
+setInterval(updateBlockColors, 60000);
+setInterval(updateTime, 60000);
+
   function updateTime() {
 
   // current day from jsday'
@@ -12,31 +17,23 @@ $(function () {
 $('#currentDay').text("It is currently " + currentTime + " on " + currentDate);
   }
 
-updateTime();
-
-setInterval(updateTime, 60000);
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
+  // TODO: Add a listener for click events on the save button. This code should use the id in the containing time-block as a key to save the user input in local storage. HINT: What does `this` reference in the click listener function? How can DOM traversal be used to get the "hour-x" id of the time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
   //
   // TODO: Add code to apply the past, present, or future class to each time block by comparing the id to the current hour. HINTS: How can the id attribute of each time-block be used to conditionally add or remove the past, present, and future classes? How can Day.js be used to get the current hour in 24-hour time?
 
   function updateBlockColors() {
     // Get the current hour in 24-hour format
-    let currentHour = dayjs().hour();  // <== This uses Day.js to get the current hour in 24-hour format
+    let currentHour = dayjs().hour();
   
-    $('.time-block').each(function() {  // <== This jQuery function loops through each element with the class 'time-block'
-      let blockHour = parseInt($(this).attr('id').split('-')[1]);  // <== This takes the 'id' attribute of the current time-block and extracts the hour
-  
+    $('.time-block').each(function() {  
+      let blockHour = parseInt($(this).attr('id'));        
       if (blockHour < currentHour) {
-        $(this).removeClass('present future').addClass('past');  // <== Adds 'past' class, removes 'present' and 'future'
+        $(this).removeClass('present future').addClass('past');  
       } else if (blockHour === currentHour) {
-        $(this).removeClass('past future').addClass('present');  // <== Adds 'present' class, removes 'past' and 'future'
+        $(this).removeClass('past future').addClass('present');  
       } else {
-        $(this).removeClass('past present').addClass('future');  // <== Adds 'future' class, removes 'past' and 'present'
+        $(this).removeClass('past present').addClass('future');  
       }
     });
   }
